57a672234bb76027993d826c42d931ff8e199339
Preston Bateman <prestonbbateman@gmail.com>
2025-03-03 17:16:55 -0600

    adding navigation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import Dropdown from "./components/Dropdown";
-import { useState } from "react";
+
 
 function App() {
-  const [selection, setSelection] = useState(null)
+  return(
+    <div>
 
-  const handleSelect = (option) => {
-    setSelection(option)
-  }
-
-const options = [
-  { label: 'Red', value: 'red' },
-  { label: 'Green', value: 'green' },
-  { label: 'Blue', value: 'blue' },
-]
-
-  return <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
+  )
 }
 
 export default App
