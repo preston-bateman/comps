103810b4f908850c61fe99c1eee7547abdd1ef1d
Preston Bateman <prestonbbateman@gmail.com>
2025-03-03 22:04:24 -0600

    fixes to css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,13 +8,18 @@ function Sidebar(){
     ]
 
     const renderedLinks = links.map((link) => {
-        return <Link key={link.label} to={link.path} className="mb-3">
+        return (<Link 
+        key={link.label} 
+        to={link.path} 
+        className="mb-3"
+        activeClassName="font-bold border-l-4 border-blue-500 pl-2"
+        >
             {link.label}
-        </Link>
+        </Link>)
     })
 
     return(
-        <div className="sticky top-0 overflow-y-scroll flex flex-col">
+        <div className="sticky top-0 flex flex-col items-start">
             {renderedLinks}
         </div>
     )
