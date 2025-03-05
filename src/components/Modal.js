76dc2eea82b275eefd9d6a2be1e6fc6a15a7c6a2
Preston Bateman <prestonbbateman@gmail.com>
2025-03-05 16:14:25 -0600

    made internal modal css changes

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,10 @@
 function Modal() {
     return (
         <div>
-
+            <div className="asbsolute inset-0 bg-gray-300 opacity-80"></div>
+            <div className="absolute inset-40 p-10 bg-white">
+                Enter your text for the Modal here...
+            </div>
         </div>
     )
 }
