10e56a250cd209377e293944f30750b83a063b7e
Preston Bateman <prestonbbateman@gmail.com>
2025-03-05 22:03:49 -0600

    finished modal components

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -5,14 +5,30 @@ import { useState } from "react"
 function ModalPage(){
     const [showModal, setShowModal] = useState(false)
 
+    const handleClose = () => { 
+        setShowModal(false)
+     }
+
     const handleClick = (first) => { 
         setShowModal(true)
     }
 
+    const actionBar = (
+        <div>
+            <Button primary onClick={handleClose}>I Accept</Button>
+        </div>
+    )
+
+    const modal = <Modal onClose={handleClose} actionBar={actionBar}>
+        <p>
+            This is where you put the text of your modal...
+        </p>
+    </Modal>
+
     return(
         <div>
             <Button primary onClick={handleClick}>Open Modal</Button>
-            {showModal && <Modal />}
+            {showModal && modal}
         </div>
     )
 }
