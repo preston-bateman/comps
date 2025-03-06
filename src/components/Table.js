9750a9d652732e08863c2a5b27f769cbe01d032e
Preston Bateman <prestonbbateman@gmail.com>
2025-03-06 14:26:54 -0600

    updated table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,30 @@
-function Table() {
+function Table({ data }) {
+
+    const renderedRows = data.map((fruit) => {
+        return (
+            <tr className="border-b" key={fruit.name}>
+                <td className="p-3">{fruit.name}</td>
+                <td className="p-3">
+                    <div className={`p-3 m-2 ${fruit.color}`}></div>
+                </td>
+                <td className="p-3">{fruit.score}</td>
+            </tr>
+        )
+    })
+
     return (
-        <div>
-            Table
-        </div>
+        <table className="table-auto border-spacing-2">
+            <thead>
+                <tr className="border-b-2">
+                    <th>Fruit</th>
+                    <th>Color</th>
+                    <th>Score</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderedRows}
+            </tbody>
+        </table>
     )
 }
 
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,9 +1,17 @@
 import Table from "../components/Table";
 
 function TablePage() {
+
+    const data = [
+        { name: 'Orange', color: 'bg-orange-500', score: 5 },
+        { name: 'Apple', color: 'bg-red-500', score: 3 },
+        { name: 'Banana', color: 'bg-yellow-300', score: 2 },
+        { name: 'Lime', color: 'bg-green-400', score: 4 },
+    ]
+
     return (
         <div>
-
+            <Table data={data} />
         </div>
     )
 }
