9dcc215124d526943bb56854bcfe0a5728a2d043
Preston Bateman <prestonbbateman@gmail.com>
2025-03-05 21:21:23 -0600

    modal fixes

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,14 @@
+import ReactDOM from 'react-dom'
+
 function Modal() {
-    return (
+    return ReactDOM.createPortal(
         <div>
             <div className="asbsolute inset-0 bg-gray-300 opacity-80"></div>
             <div className="absolute inset-40 p-10 bg-white">
                 Enter your text for the Modal here...
             </div>
-        </div>
+        </div>,
+        document.querySelector('.modal-container')
     )
 }
 
