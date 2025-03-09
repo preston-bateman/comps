36dd9cbee9ddcc697dba9eeab9b9e75c44abe662
Preston Bateman <prestonbbateman@gmail.com>
2025-03-08 19:31:58 -0600

    sort function started on table

--- src/components/Table.js
+++ src/components/Table.js
@@ -32,3 +32,5 @@ function Table({ data, config, keyFn }) {
 }
 
 export default Table
+
+//Remember to add the sort functionality to the table so that it is able to be sorted no matter what is added in.
